43b4e9c5552b4050edb546485adc48936ee747a9
danielscoffee <danielrsantoscontato@gmail.com>
2026-03-28 20:52:03 -0300

    feat: nix for consistent enviroment

---
+++ flake.nix
@@ -0,0 +1,49 @@
+{
+  description = "A flake enviroment for better devex";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          config.allowUnfree = true;
+        };
+
+        commonPackages = with pkgs; [
+          bashInteractive
+          coreutils
+          curl
+          findutils
+          gawk
+		  git
+          gnugrep
+          gnumake
+          jq
+          unzip
+          zip
+          docker-client
+		  docker-compose
+        ];
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = commonPackages;
+
+          shellHook = '''';
+        };
+
+        formatter = pkgs.nixpkgs-fmt;
+
+        apps = {
+          shell = {
+            type = "app";
+            program = "${pkgs.bash}/bin/bash";
+          };
+        };
+      });
+}
